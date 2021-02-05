7bb66cb827d1823d35bf063e379ca11a7da3ff0a
akshay-narkar <akshaynarkar1295@gmail.com>
2021-02-05 20:06:29 +0530

    Add helper method to views, add username to registration, add Post.new in controller for new action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def signin?(post)
+    if user_signed_in?
+      post.user.username
+    else
+      'Anon'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_075753) do
+ActiveRecord::Schema.define(version: 2021_02_05_132356) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_075753) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
